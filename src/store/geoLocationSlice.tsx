d8d2ee29fe014127dce125fd913e4132e0bafd91
Bhavya Bharti <bhavyab@outlook.com>
2022-06-03 19:15:53 -0700

    error handled

--- src/store/geoLocationSlice.tsx
+++ src/store/geoLocationSlice.tsx
@@ -3,12 +3,22 @@ import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { initialState } from './initialState';
 import {getTimeZone, convertDateAndTime} from './apiCalls/getTime';
 import { RootState } from './store';
+import { useAppDispatch } from './hooks'
 
 export const getTimeAction = createAsyncThunk(
-    'geoLocaterState/getTimeZone', async (payload: {lattitude: number, longitude: number}) => {
-        return await getTimeZone(payload.lattitude, payload.longitude);
-    })
-
+  "geoLocaterState/getTimeZone",
+  async (
+    payload: { lattitude: number; longitude: number },
+    { rejectWithValue }
+  ) => {
+      try {
+          return await getTimeZone(payload.lattitude, payload.longitude);
+      } catch (error) {
+        return rejectWithValue(error);
+      }
+  }
+);
+// const dispatch = useAppDispatch()
 export const geoLocaterSlice = createSlice({
     name : 'geoLocaterState',
     initialState,
@@ -18,11 +28,13 @@ export const geoLocaterSlice = createSlice({
         builder.addCase(getTimeAction.fulfilled, (state, action: PayloadAction<any>) => {
             state = {...state, ...action.payload};
             state.currentTime = convertDateAndTime(action.payload.timestamp);
+            return {...state};
+        });
+        builder.addCase( getTimeAction.rejected, (state, action: PayloadAction<any>) => {
+            state = { ...state, ...action.payload };
+            state.currentTime = convertDateAndTime(action.payload.timestamp);
             return state;
-        });
-        builder.addCase(getTimeAction.rejected, (state) => {
-          console.log("rejected");
-        });
+          });
     }
 });
 
